cd3cdf4e3aaf512e402dfa03fcbf81907f7ad071
Blaise Papa <41106147+Blvisse@users.noreply.github.com>
2024-01-31 11:12:00 +0300

    Create predict.py

---
+++ predict.py
@@ -0,0 +1,19 @@
+import pandas 
+import numpy
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+print("Reading Environmental Variables")
+aws_access_key=os.getenv("AWS_S3_PACS_ACCESS_KEY")
+aws_secret_key=os.getenv("AWS_S3_PACS_SECRET_KEY")
+pacs_url=os.getenv("URL")
+
+if (aws_access_key == None) or (aws_secret_key == None):
+    print("Failed to access AWS ACCESS KEYS Terminating Program")
+    exit(1)
+elif pacs_url == None:
+    print("Failed to fetch PACS end point, Terminating Program")
+    exit(1)
+else:
+    print("Successfully Retrieved AWS Access Key and PACS end point")
